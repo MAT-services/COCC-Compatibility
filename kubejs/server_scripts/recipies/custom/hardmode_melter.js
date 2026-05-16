f0e1311ddb65236836a3b8ee7247c4e37dce0013
MAT Services Administrator <157519566+MAT-services@users.noreply.github.com>
2026-05-16 14:42:55 +0200

    Update FancyMenu title screen layout
    Add ConfigurationScreen entries and adjust title-screen layout: update timestamps and many element identifiers, remap executable blocks/loading requirement IDs, rename instance_identifiers, reposition and resize multiple buttons/widgets, and toggle visibility states. Add a server-IP based loading requirement for a button. Also add a new options_screen_layout file and update FancyMenu options, and modify the KubeJS hardmode_melter recipe script.

--- kubejs/server_scripts/recipies/custom/hardmode_melter.js
+++ kubejs/server_scripts/recipies/custom/hardmode_melter.js
@@ -4,6 +4,44 @@ ServerEvents.recipes(event => {
         "titanium_melter"
     ]
     hardmode_melters.forEach(hardmode_melter => {
+        /* // DO NOT WRKS
+        event.forEachRecipe({type: 'slag:melting'}, recipe => {
+            let inputs = recipe.getIngredients().map(i => {
+                return { item: i.getItems()[0].id, amount: 1 }
+            })
+
+            event.custom({
+                type: "custommachinery:custom_machine",
+                machine: `custommachinery:${hardmode_melter}`,
+                time: 100,
+                requirements: [
+                    {
+                        type: "custommachinery:item",
+                        mode: "input",
+                        id: "input1",
+                        ingredient: {
+                            item: `${recipe.getIngredient().tag}`,
+                        },
+                        amount: 1
+                    },
+                    {
+                        type: "custommachinery:fuel",
+                        amount: 1
+                    },
+                    {
+                        "type": "custommachinery:fluid",
+                        "mode": "output",
+                        "id": "output1",
+                        "ingredient": {
+                            "fluid": `${recipe.getResult().id}`,
+                            "amount": recipe.getResult().amount
+                        },
+                    }
+                ]
+            }).id(`cocc:${hardmode_melter}_melter/${recipe.getIngredient().tag}/${recipe.getResult().id}`);
+        })
+        */
+
         event.custom({
             type: "custommachinery:custom_machine",
             machine: `custommachinery:${hardmode_melter}`,
