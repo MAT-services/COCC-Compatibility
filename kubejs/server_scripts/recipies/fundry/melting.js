c1f9112abf5f03b3614e9f564fab9c5695ac2cd5
MAT Services Administrator <mat.stocage@gmail.com>
2026-05-19 14:12:42 +0200

    Normally, melting recipes are list-powered now.

--- kubejs/server_scripts/recipies/fundry/melting.js
+++ kubejs/server_scripts/recipies/fundry/melting.js
@@ -6,51 +6,68 @@ ServerEvents.recipes(event => {
         {recipe: "melting", type: "slag"}
     ]
     let materials = [
-        {id: "iron", ingot: "minecraft:iron_ingot"}
+        {id: "iron", level: 0, ingot: "minecraft:iron_ingot", fluid: "slag:molten_iron"}
     ]
     let materialTypes = [
-        "ingot",
-        "block",
-        "nuget",
-        "dust",
-        "gem"
+        {id: "ingot", coef: 72},
+        {id: "block", coef: 648},
+        {id: "nugget", coef: 8},
+        {id: "dust", coef: 72},
+        {id: "small_dust", coef: 8},
+        {id: "gem", coef: 80}
     ]
-    melters.forEach(melter => {
-        if (melter.type==="cm") {
-            materials.forEach(material => {
-                event.custom({
-                    type: "custommachinery:custom_machine",
-                    machine: `custommachinery:${melter}`,
-                    time: 100,
-                    requirements: [
-                        {
-                            type: "custommachinery:item",
-                            mode: "input",
-                            id: "input1",
-                            ingredient: {
-                                item: `${material.ingot}`
+
+    materials.forEach(material => {
+        materialTypes.forEach(materialType => {
+            melters.forEach(melter => {
+                if (melter.type==="cm") {if (material.level < 2) {
+                    event.custom({
+                        type: "custommachinery:custom_machine",
+                        machine: `custommachinery:${melter.recipe}`,
+                        time: 100,
+                        requirements: [
+                            {
+                                type: "custommachinery:item",
+                                mode: "input",
+                                id: "input1",
+                                ingredient: {
+                                    tag: `c:${materialType.id}s/${material.id}`
+                                },
+                                amount: 1
                             },
-                            amount: 1
-                        },
-                        {
-                            type: "custommachinery:fuel",
-                            amount: 1
-                        },
-                        {
-                            "type": "custommachinery:fluid",
-                            "mode": "output",
-                            "id": "output1",
-                            "ingredient": {
-                                "fluid": `slag:molten_${material.id}`,
-                                "amount": 72
+                            {
+                                type: "custommachinery:fuel",
+                                amount: 1
                             },
-                        }
-                    ]
-                }).id(`cocc:${melter.recipe}/molten_${material.ingot}`);
-            })
-        }
-        if (melter.type==="slag") {
-            ///SNE recipe
-        }
+                            {
+                                "type": "custommachinery:fluid",
+                                "mode": "output",
+                                "id": "output1",
+                                "ingredient": {
+                                    "fluid": `${material.fluid}`,
+                                    "amount": materialType.coef
+                                }
+                            }
+                        ]
+                    }).id(`cocc:${melter.recipe}/molten_${material.id}-from-${materialType.id}`);
+                }}
+                if (melter.type==="slag") {if (material.level < 1) {
+                    event.custom({
+                        "type": `slag:${melter.recipe}`,
+                        "ingredient": {
+                            "tag": `c:${materialType.id}s/${material.id}` 
+                        },
+                        "result": [
+                            {
+                            "amount": materialType.coef,
+                            "id": `${material.fluid}` 
+                            }
+                        ]
+                    }).id(`cocc:${melter.recipe}/molten_${material.id}-from-${materialType.id}`);
+                }}
+            });
+        })
+        
+        // OTHERS
     });
 })
