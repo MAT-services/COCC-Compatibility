5087d8c1c3f2fb9031e326b2c1b9c78b3f7589a9
MAT Services Administrator <mat.stocage@gmail.com>
2026-05-27 10:29:14 +0200

    Even more optimization ? (To test.)

--- kubejs/server_scripts/recipes/fundry/auto.js
+++ kubejs/server_scripts/recipes/fundry/auto.js
@@ -6,8 +6,8 @@ ServerEvents.recipes(event => {
     ]
     let materials = [
         {id: "iron", level: 0, fluid: "slag:molten_iron", ingot: "minecraft:iron_ingot", storage_block: "minecraft:iron_block", nugget: "minecraft:iron_nugget", dust: "oritech:iron_dust", gem: "oritech:iron_gem"},
-        {id: "gold", level: 0, fluid: "slag:molten_gold", ingot: "minecraft:iron_ingot", storage_block: "minecraft:gold_block", nugget: "minecraft:gold_nugget", dust: "oritech:gold_dust", gem: "oritech:gold_gem"},
-        {id: "copper", level: 0, fluid: "slag:molten_copper", ingot: "minecraft:iron_ingot", storage_block: "minecraft:copper_block", nugget: "create:copper_nugget", dust: "oritech:copper_dust", gem: "oritech:copper_gem"},
+        {id: "gold", level: 0, fluid: "slag:molten_gold", ingot: "minecraft:gold_ingot", storage_block: "minecraft:gold_block", nugget: "minecraft:gold_nugget", dust: "oritech:gold_dust", gem: "oritech:gold_gem"},
+        {id: "copper", level: 0, fluid: "slag:molten_copper", ingot: "minecraft:copper_ingot", storage_block: "minecraft:copper_block", nugget: "create:copper_nugget", dust: "oritech:copper_dust", gem: "oritech:copper_gem"},
         {id: "steel", level: 0, fluid: "kubejs:molten_steel", ingot: "oritech:steel_ingot", storage_block: "oritech:steel_block", nugget: "empty", dust: "oritech:steel_dust", gem: "empty"},
         {id: "platinum", level: 0, fluid: "kubejs:molten_platinum", ingot: "confluence:platinum_ingot", storage_block: "confluence:platinum_block", nugget: "confluence:platinum_nugget", dust: "oritech:platinum_dust", gem: "empty"}
     ]
@@ -23,119 +23,61 @@ ServerEvents.recipes(event => {
     materials.forEach(material => {
         itemTypes.forEach(itemType => {
             if (material[itemType.id]!="empty") {
-                function getItem(ingredient) {
-                    if (ingredient.type==="IV") {
-                        
+                function getItem(from) { // Potentially add `"count": 1` if the SNE recipes do not work.
+                    if (itemType.type==="VI") {
+                        if (from==="output") {return {item: material[itemType.id]/* VERIFY IF ID / ITEM works for SNE and MJS together. */}}; if (from==="input") {return {tag: `c:${itemType.id}s/${material.id}`}}
+                    }
+                    if (itemType.type==="DSE") {
+                        return {item: `slag:dynamic_part`,"components": {"slag:material_type": `slag:${material.id}`,"slag:part_type": `slag:${itemType.id}`}}
                     }
                 }
 
                 melters.forEach(melter => {
-                    if (itemType.type==="VI") {
-                        if (melter.type==="cm") {if (material.level < 3) {
-                            event.custom({
-                                type: "custommachinery:custom_machine",
-                                machine: `custommachinery:${melter.recipe}`,
-                                time: 100,
-                                requirements: [
-                                    {
-                                        type: "custommachinery:item",
-                                        mode: "input",
-                                        id: "input1",
-                                        ingredient: {
-                                            tag: `c:${itemType.id}s/${material.id}`
-                                        },
-                                        amount: 1
-                                    },
-                                    {
-                                        type: "custommachinery:fuel",
-                                        amount: 1
-                                    },
-                                    {
-                                        "type": "custommachinery:fluid",
-                                        "mode": "output",
-                                        "id": "output1",
-                                        "ingredient": {
-                                            "fluid": material.fluid,
-                                            "amount": itemType.coef
-                                        }
-                                    }
-                                ]
-                            }).id(`cocc:${melter.recipe}/molten_${material.id}-from-${itemType.id}`);
-                        }}
-                        if (melter.type==="slag") {if (material.level < 1) {
-                            event.custom({
-                                "type": `slag:${melter.recipe}`,
-                                "ingredient": {
-                                    "tag": `c:${itemType.id}s/${material.id}` 
+                    if (melter.type==="cm") {if (material.level < 3) {
+                        event.custom({
+                            type: "custommachinery:custom_machine",
+                             machine: `custommachinery:${melter.recipe}`,
+                            time: 100,
+                            requirements: [
+                                {
+                                    type: "custommachinery:item",
+                                    mode: "input",
+                                    id: "input1",
+                                    ingredient: getItem("input"),
+                                    amount: 1
                                 },
-                                "ingredients": [],
-                                "result": [
-                                    {
+                                {
+                                    type: "custommachinery:fuel",
+                                    amount: 1
+                                },
+                                {
+                                    "type": "custommachinery:fluid",
+                                    "mode": "output",
+                                    "id": "output1",
+                                    "ingredient": {
+                                        "fluid": material.fluid,
+                                        "amount": itemType.coef
+                                    }
+                                }
+                            ]
+                        }).id(`cocc:${melter.recipe}/molten_${material.id}-from-${itemType.id}`);
+                    }}
+                    if (melter.type==="slag") {if (material.level < 1) {
+                        event.custom({
+                            "type": `slag:${melter.recipe}`,
+                            "ingredient": {
+                                "tag": `c:${itemType.id}s/${material.id}` 
+                            },
+                            "ingredients": [],
+                            "result": [
+                                {
                                     "amount": itemType.coef,
                                     "id": material.fluid
-                                    }
-                                ]
-                            }).id(`cocc:${melter.recipe}/molten_${material.id}-from-${itemType.id}`);
-                        }}
-                    }
-                    if (itemType.type==="DSE") {
-                        if (melter.type==="cm") {if (material.level < 3) {
-                            event.custom({
-                                type: "custommachinery:custom_machine",
-                                machine: `custommachinery:${melter.recipe}`,
-                                time: 100,
-                                requirements: [
-                                    {
-                                        type: "custommachinery:item",
-                                        mode: "input",
-                                        id: "input1",
-                                        ingredient: {
-                                            item: `slag:dynamic_part`,
-                                            "components": {
-                                                "slag:material_type": `slag:${material.id}`,
-                                                "slag:part_type": `slag:${itemType.id}`
-                                            }
-                                        },
-                                        amount: 1
-                                    },
-                                    {
-                                        type: "custommachinery:fuel",
-                                        amount: 1
-                                    },
-                                    {
-                                        "type": "custommachinery:fluid",
-                                        "mode": "output",
-                                        "id": "output1",
-                                        "ingredient": {
-                                            "fluid": material.fluid,
-                                            "amount": itemType.coef
-                                        }
-                                    }
-                                ]
-                            }).id(`cocc:${melter.recipe}/molten_${material.id}-from-${itemType.id}`);
-                        }}
-                        if (melter.type==="slag") {if (material.level < 1) {
-                            event.custom({
-                                "type": `slag:${melter.recipe}`,
-                                "ingredient": {
-                                    "item": `slag:dynamic_part`,
-                                    "components": {
-                                        "slag:material_type": `slag:${material.id}`,
-                                        "slag:part_type": `slag:${itemType.id}`
-                                    }
-                                },
-                                "ingredients": [],
-                                "result": [
-                                    {
-                                    "amount": itemType.coef,
-                                    "id": material.fluid
-                                    }
-                                ]
-                            }).id(`cocc:${melter.recipe}/molten_${material.id}-from-${itemType.id}`);
-                        }}
-                    }
-                });
-
+                                }
+                            ]
+                        }).id(`cocc:${melter.recipe}/molten_${material.id}-from-${itemType.id}`);
+                    }}
+                })
                 // Bassin Casting
                 if (itemType.cast==="basin") {
                     event.custom({
@@ -152,37 +94,15 @@ ServerEvents.recipes(event => {
                 }
                 // Table Casting
                 if (itemType.cast==="table") {
-                    if (itemType.type==="VI") {
-                        event.custom({
-                            "type": "slag:table_casting",
-                            "cast": `slag:cast/${itemType.id}s`,
-                            "ingredient": {
-                                "amount": itemType.coef,
-                                "id": material.fluid
-                            },
-                            "result": {
-                                "count": 1,
-                                "id": material[itemType.id]
-                            }
-                        }).id(`cocc:table_casting/${material.id}_${itemType.id}`);
-                    }; if (itemType.type==="DSE") {
-                        event.custom({
-                            "type": "slag:table_casting",
-                            "cast": `slag:cast/${itemType.id}s`,
-                            "ingredient": {
-                                "amount": itemType.coef,
-                                "id": material.fluid
-                            },
-                            "result": {
-                                "count": 1,
-                                "id": "slag:dynamic_part",
-                                "components": {
-                                    "slag:material_type": `slag:${material.id}`,
-                                    "slag:part_type": `slag:${itemType.id}`
-                                }
-                            }
-                        })
-                    }
+                    event.custom({
+                        "type": "slag:table_casting",
+                        "cast": `slag:cast/${itemType.id}s`,
+                        "ingredient": {
+                            "amount": itemType.coef,
+                            "id": material.fluid
+                        },
+                        "result": getItem("output")
+                    }).id(`cocc:table_casting/${material.id}_${itemType.id}`);
                 }
             }
         })
