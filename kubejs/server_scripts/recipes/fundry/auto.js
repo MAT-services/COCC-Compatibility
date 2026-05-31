56335d82f319c146f09dfc39994975096d5252ac
MAT Services Administrator <157519566+MAT-services@users.noreply.github.com>
2026-05-31 16:58:26 +0200

    Update auto.js

--- kubejs/server_scripts/recipes/fundry/auto.js
+++ kubejs/server_scripts/recipes/fundry/auto.js
@@ -1,5 +1,6 @@
 // Also add ways to automatically melt raw materials.
 // Test the DnE recipes generation doesn't work correctly.
+// CHECK THE LINE 43 OF CUSTOM.JS TO UNDERSTAND WHAT GO WRONG WITH DSE !!!!!
 
 ServerEvents.recipes(event => {
     let melters = [
