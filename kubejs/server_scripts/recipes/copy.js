b0e749ed76ff457dd57a3353781a3b3e46026713
MAT Services Administrator <157519566+MAT-services@users.noreply.github.com>
2026-07-23 10:56:19 +0000

    Now, everithing should work the best.

--- kubejs/server_scripts/recipes/copy.js
+++ kubejs/server_scripts/recipes/copy.js
@@ -1,24 +1,20 @@
 ServerEvents.recipes(event => {
     event.forEachRecipe({ type: "terra_curio:workshop" }, recipe => {
-        const input = JSON.parse(`${recipe.json}`).ingredients;
-        input.unshift("kubejs:workshop_mixer_adapter");
-        const output = [JSON.parse(`${recipe.json}`).result];
-        output.unshift("kubejs:workshop_mixer_adapter");
+        const GotJSON = JSON.parse(`${recipe.json}`);
+        const input = GotJSON.ingredients;
+        input.push("kubejs:workshop_mixer_adapter");
+        const output = [GotJSON.result];
+        output.push("kubejs:workshop_mixer_adapter");
 
         event.recipes.create.mixing(output, input)
     }); 
 
     event.forEachRecipe({ type: "confluence:dye_vat" }, recipe => {
-        const output = [JSON.parse(`${recipe.json}`).result, {"id": "kubejs:dye_filter"}];
-        const input = [];
-        const lenght = JSON.parse(`${recipe.json}`).ingredients.lenght;
+        const GotJSON = JSON.parse(`${recipe.json}`);
+        const output = [GotJSON.result, {"id": "kubejs:dye_filter"}];
+        const input = [{"item": "kubejs:dye_filter"}];
 
-        let n = 0;
-        while (n<lenght) {
-            input.unshift(JSON.parse(`${recipe.json}`).ingredients[x]);
-            n++;
-        };
-
+        for (const i of GotJSON.ingredients) {input.push(GotJSON.ingredients[i]);};
         event.custom({
             "type": "create:mixing",
             "ingredients": input,
